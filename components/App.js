189e6e1b5211a970dfc1d8fa91e4e2622d943c02
Shawn M <44383059+Solcodeguy@users.noreply.github.com>
2021-01-05 00:54:13 -0500

    Initial commit

---
+++ components/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import TodoItem from "./TodoItem"
+import todosData from "./todosData"
+
+function App() {
+    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+    
+    return (
+        <div className="todo-list">
+            {todoItems}
+        </div>
+    )
+}
+
+export default App
